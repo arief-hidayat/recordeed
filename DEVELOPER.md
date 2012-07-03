9c9bdd76e20c5a32b77195490951f324ce191527
Arief Hidayat <mr.arief.hidayat@gmail.com>
2012-07-04 02:19:03 +0800

    developer note

---
+++ DEVELOPER.md
@@ -0,0 +1,17 @@
+# Development Environment
+
+My development environments:
+
+* JDK 1.7
+* Grails 2.0.3
+* Git
+* IntelliJ IDEA (should be able to be replaced by SpringSource TS)
+
+
+# Testing
+
+Testing tools are mostly from Grails plugins:
+
+* jUnit & Spock (for unit test)
+* Cucumber (for functional test)
+* Node + Cucumber-js (for testing cucumber)
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,6 +43,8 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
+//        compile ":angular-scaffolding:1.0-SNAPSHOT"
+
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
